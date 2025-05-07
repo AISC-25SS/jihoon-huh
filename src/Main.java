3d61d91a78aedb099e255536dd3dc1b1fe77c212
DESKTOP-5FU4EFM\Home <huhjhace@gmail.com>
2025-05-07 18:39:24 +0900

    BLOCK

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,7 @@ import test.GitTest;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-
-        GitTest gitTest = new GitTest();
-
-        gitTest.print_message();
+        System.out.println("왜안되냐고");
 
     }
 }
